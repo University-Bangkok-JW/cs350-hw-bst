b95df9a80b46dbd587c135ccc6dfaed97d4be459
JetsadaWijit <jetsadawijit@outlook.com>
2024-10-29 11:41:47 +0000

    Create m100.py, init class

---
+++ m100.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if not self.root:
+            self.root = Node(value)
+        else:
+            self.insertNode(self.root, value)
+
+    def insertNode(self, cur, value):
+        if value < cur.value:
+            if cur.left:
+                self.insertNode(cur.left, value)
+            else:
+                cur.left = Node(value)
+        else:
+            if cur.right:
+                self.insertNode(cur.right, value)
+            else:
+                cur.right = Node(value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
